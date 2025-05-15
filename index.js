7d32aef6ef666a69c6796e722dca4338a857897c
Marcosrex777 <105959809+Marcosrex777@users.noreply.github.com>
2025-05-15 01:19:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
